1d16fe73767ae60d586e1308962cbaf67df6c5ae
bossy <jeff@passedpawn.com>
2024-07-15 21:36:55 -0400

    initial commit

---
+++ install-scripts/git-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+git config --global init.defaultBranch trunk
+git config --global user.name $USER
+git config --global user.email "jeff@passedpawn.com"
+git config --global core.editor=vim
+git config --global core.autocrlf=input
+git config --global --list
