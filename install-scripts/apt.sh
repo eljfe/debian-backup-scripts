a884f42903334eaea942875df91289bd109807af
bossy <jeff@passedpawn.com>
2025-01-19 02:03:07 -0500

    echo each install; added write to redshift config file settings

--- install-scripts/apt.sh
+++ install-scripts/apt.sh
@@ -6,11 +6,40 @@ echo "--> This script needs to be called like so:"
 echo "         'sudo ${USER} ${0}'"
 source proceed-conditional.sh
 
+echo "--> updating... upgrading"
 apt update
 apt upgrade
+
+echo "--> installing nala"
 apt install nala
+echo "--> installing git xclip build-essential tree"
 nala install git xclip build-essential tree
-nala install papirus-icon-theme redshift
+
+echo "--> installing papirus-icon-theme"
+nala install papirus-icon-theme 
+
+echo "--> installing neofetch tmux bluetooth"
 nala install neofetch tmux bluetooth
+
+echo "--> installing nmap"
 nala install nmap
 
+echo "--> installing redshift"
+nala install redshift redshift-gtk
+
+echo "--> copying settings into ~/.config/redshift.config"
+cat << EOF > ~/.config/redshift.conf                                                          
+[redshift]
+; Set the day and night screen temperatures
+temp-day=3350
+temp-night=3350
+fade=1
+location-provider=manual
+
+[manual]
+lat=48.1
+lon=11.6
+
+[randr]
+screen=0 
+EOF
