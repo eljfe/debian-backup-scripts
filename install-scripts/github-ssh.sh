331baca22c0168cf5a058003f1b5f925bb492c24
bossy <jeff@passedpawn.com>
2025-04-07 02:08:17 -0400

    directory creation feature; choose user feature; still fails on eval ssh-agent... tho

--- install-scripts/github-ssh.sh
+++ install-scripts/github-ssh.sh
@@ -3,27 +3,57 @@ set -e	# exit any non-successful executions
 # set -x	# debugging: echo commands as you go
 
 echo "--> This script needs to be called like so:"
-echo "         'sudo -u ${USER} ${0}'"
-# read -r -p "... Proceed? (Y/n) ... " go
-# pat='y$|Y$|^$'
-# if [[ ! $go =~ $pat ]]; then
-	# echo "--> bye"
-	# exit
-# fi
+echo "         'sudo -u  ${0}'"
 source proceed-conditional.sh
+$(proceed)
 
-sshdir=${HOME}"/.ssh/"
+
+##################################
+## build an array of user names ##
+##################################
+# 1st get the userland usernames from /etc/passwd - separated by spaces
+ust=$(cat /etc/passwd | grep home | awk -F':' '{print $1}' | sed 'N;s/\n/ /g')
+# 2nd space separated list to array ${ulist}
+IFS=' ' read -r -a ulist <<< "$ust"
+# fancy itemized listing looper
+# https://stackoverflow.com/a/9718667
+# $((${#ulist}[*]-1));   <-- what is this!?
+# $(( blahblah -1)) is the 'do math' part
+# ${blah} is the string reference
+# ${#blah[*]} is the numeric length of the array
+echo "--> choose a freakin' user from this list (sorry grumpy today), "
+for i in $(seq 0 $((${#ulist[*]}-1))); do
+	echo $((1+${i})). ${ulist[i]} 
+done 
+read -r -p "... plz ...   " choice
+choice=$((${choice}-1))
+usrname=${ulist[${choice}]}
+
+sshdir=/home/${usrname}"/.ssh/"
 newkey="github_ed25519"
 scriptdir=$(pwd)
 
+# sudo -u ${usrname} 
+
+# .ssh directory exists test
+if [ ! -f ${sshdir} ]; then
+	echo "--> the ssh config folder is missing from you home directory,"
+	echo "         ${sshdir}"
+	echo "    ... and no proceeding without it.  Shall I make it now?"
+	$(proceed)
+	sudo -u ${usrname} mkdir -p ${sshdir}
+	echo "    ... done"
+fi
+
 # interactive ssh key generation
 echo "--> making your new key"
-ssh-keygen -t ed25519 -C "jeff@passedpawn.com" -f "${sshdir}${newkey}" -N '' <<< $'\ny' >/dev/null 2>&1
+# echo "TEST ${sshdir}${newkey}"
+sudo -u ${usrname} ssh-keygen -t ed25519 -C "jeff@passedpawn.com" -f "${sshdir}${newkey}" -N '' <<< $'\ny' >/dev/null 2>&1
 echo "--> key made"
 echo "--> registering your key"
-eval "$(ssh-agent -s)"
-ssh-add $newkey
-cat << EOF >> ${sshdir}config 
+sudo -u ${usrname} eval "$(sudo -u ${usrname} ssh-agent -s)"
+sudo -u ${usrname} ssh-add $newkey
+sudo -u ${usrname} cat << EOF >> ${sshdir}config 
 Host github.com
 	User git
 	IdentityFile ${sshdir}${newkey}
